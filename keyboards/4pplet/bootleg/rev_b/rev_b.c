e6dca894ef4d3344cc140ef7b9b4bbabc3020953
4pplet <4pplet@protonmail.com>
2021-03-13 12:48:44 +0100

    cont

--- keyboards/4pplet/bootleg/rev_b/rev_b.c
+++ keyboards/4pplet/bootleg/rev_b/rev_b.c
@@ -1,1 +1,21 @@
 #include "rev_b.h"
+
+void matrix_init_kb(void) {
+    // put your keyboard start-up code here
+    // runs once when the firmware starts up
+
+    setPinOutput(CAPS_PIN);
+    setPinOutput(SCROLL_PIN);
+    setPinOutput(NUM_PIN);
+    matrix_init_user();
+}
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res && LOCK_LIGHTS) {
+        writePin(CAPS_PIN, led_state.caps_lock);
+        writePin(SCROLL_PIN, led_state.scroll_lock);
+        writePin(NUM_PIN, led_state.num_lock);
+    }
+    return res;
+}
--- keyboards/4pplet/bootleg/rev_b/rules.mk
+++ keyboards/4pplet/bootleg/rev_b/rules.mk
@@ -13,6 +13,7 @@ COMMAND_ENABLE = yes    # Commands for debug and configuration
 # SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	      # USB Nkey Rollover
 RGBLIGHT_ENABLE = yes
+# BACKLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
 ENCODER_ENABLE = yes
 
