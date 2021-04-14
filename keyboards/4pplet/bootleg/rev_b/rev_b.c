b947412805142a29a6360fccaec7fe5dd1f91d1e
4pplet <4pplet@protonmail.com>
2021-04-14 23:50:31 +0200

    Update rev_b.c

--- keyboards/4pplet/bootleg/rev_b/rev_b.c
+++ keyboards/4pplet/bootleg/rev_b/rev_b.c
@@ -16,9 +16,21 @@ void matrix_init_kb(void) {
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if(res && LOCK_LIGHTS) {
-        writePin(CAPS_PIN, led_state.caps_lock);
-        writePin(SCROLL_PIN, led_state.scroll_lock);
-        writePin(NUM_PIN, led_state.num_lock);
+        if(led_state.caps_lock){
+            writePin(CAPS_PIN, 0);
+        }
+        else
+            writePin(CAPS_PIN, 1);
+        if(led_state.scroll_lock){
+            writePin(SCROLL_PIN, 0);
+        }
+        else
+            writePin(SCROLL_PIN, 1);
+        if(led_state.num_lock){
+            writePin(NUM_PIN, 0);
+        }
+        else
+            writePin(NUM_PIN, 1);
     }
     return res;
 }
